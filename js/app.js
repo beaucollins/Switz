5a71fac7d86db4dd0403147aa60f0da9a38ffae8
Beau Collins <beaucollins@gmail.com>
2010-11-19 10:05:19 -0800

    player sample

--- js/app.js
+++ js/app.js
@@ -31,6 +31,75 @@ $.fn.storeHeight = function(){
 
   }
   
+  $.initializePlayer = function(player){
+    
+    // setup the scrollbar if necessary
+    var sidebar = player.find('#player_sidebar');
+    // the amount of the area that is viewable as a percentage
+    var viewable_ratio = function(){
+      return sidebar.height() / sidebar.attr('scrollHeight');
+    }
+    // the amount of pixels we can scroll, changes when images are loaded
+    
+    var scrollable_height = function(){
+      return sidebar.attr('scrollHeight') - sidebar.height();
+    }
+    var scrolled = 0;
+    
+    var mouse_x = 0;
+    var mouse_y = 0;
+    
+    var scrollbar = $('<div id="player_scrollbar"></div>')
+                        .insertAfter(sidebar)
+                        .css({
+                          position:'absolute',
+                          top:'5px',
+                          right: '5px',
+                          width: '5px',
+                          height: viewable_ratio() * sidebar.height(),
+                          background: '#FFF',
+                          opacity: 0.6,
+                          borderRadius: '2px'
+                        })
+                        .mousedown(function(e){
+                          mouse_x = e.screenX;
+                          mouse_y = e.screenY;
+                          $('body').bind('mousemove', mouseFollower);
+                          $('body').one('mouseup', function(e){
+                            $('body').unbind('mousemove', mouseFollower);
+                          })
+                        });
+                        
+    var mouseFollower = function(e){
+      e.preventDefault();
+      var moved = e.screenY - mouse_y;
+      var new_top = (scrollbar.position()['top'] + moved - 5)/viewable_ratio();
+      var t = sidebar.scrollTop();
+      sidebar.scrollTop(new_top);
+      if(t != sidebar.scrollTop()){
+        mouse_y = e.screenY;
+      }
+    }
+    
+    sidebar.scroll(function(e){
+      var r = viewable_ratio();
+      var h = (sidebar.height() - 10) * r;
+      scrolled = sidebar.attr('scrollTop'), scrollable_height();
+      
+      scrollbar.css({
+        height: h,
+        top: (scrolled * r) + 5
+      })
+    });
+    
+    sidebar.mousewheel(function(e, delta){
+      e.preventDefault();
+      sidebar.scrollTop(sidebar.scrollTop() - (delta * 2));
+    });
+    
+    
+  }
+  
 })();
 
 
